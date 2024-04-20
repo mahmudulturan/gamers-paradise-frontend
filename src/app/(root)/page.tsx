a7726b8605668094f74e02a3acdfd6a6b2837843
mahmudulturan <mahmudulhasanturan@gmail.com>
2024-04-20 16:07:57 +0600

    chore: root layout resized

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -5,16 +5,6 @@ const HomePage = () => {
     return (
         <div>
             HomePage
-            <div className="min-h-screen">
-                <Button variant={"ghost"}>CLick me</Button>
-                lorem5000
-            </div>
-            <div className="min-h-screen">
-                lorem5000
-            </div>
-            <div className="min-h-screen">
-                lorem5000
-            </div>
         </div>
     );
 };
