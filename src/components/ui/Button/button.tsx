e1eae3f6a818f098090fc55af5c2b7346f9ad0ff
mahmudulturan <mahmudulhasanturan@gmail.com>
2024-04-21 23:20:25 +0600

    add: new secondery variant of button component

--- src/components/ui/Button/button.tsx
+++ src/components/ui/Button/button.tsx
@@ -7,17 +7,17 @@ import { cn } from "@/lib/utils"
 import './button.css'
 
 const buttonVariants = cva(
-  "btn inline-flex items-center justify-center whitespace-nowrap rounded-md text-xl font-bold uppercase ring-offset-white transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-slate-950 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 dark:ring-offset-slate-950 dark:focus-visible:ring-slate-300",
+  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-xl font-bold uppercase ring-offset-white transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-slate-950 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 dark:ring-offset-slate-950 dark:focus-visible:ring-slate-300",
   {
     variants: {
       variant: {
-        default: "bg-primaryCol text-textCol hover:bg-textCol hover:text-primaryCol text-lg dark:bg-slate-50 dark:text-slate-900 dark:hover:bg-slate-50/90",
+        default: "btn-default bg-primaryCol text-textCol hover:bg-textCol hover:text-primaryCol text-lg",
         destructive:
           "bg-red-500 text-slate-50 hover:bg-red-500/90 dark:bg-red-900 dark:text-slate-50 dark:hover:bg-red-900/90",
         outline:
           "border border-slate-200 bg-white hover:bg-slate-100 hover:text-slate-900 dark:border-slate-800 dark:bg-slate-950 dark:hover:bg-slate-800 dark:hover:text-slate-50",
         secondary:
-          "bg-slate-100 text-slate-900 hover:bg-slate-100/80 dark:bg-slate-800 dark:text-slate-50 dark:hover:bg-slate-800/80",
+          "btn-secondery bg-textCol text-primaryCol hover:bg-primaryCol hover:text-textCol text-lg",
         ghost: "hover:bg-slate-100 hover:text-slate-900 dark:hover:bg-slate-800 dark:hover:text-slate-50",
         link: "text-slate-900 underline-offset-4 hover:underline dark:text-slate-50",
       },
