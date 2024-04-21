7cb9982f00f0182f3ee6a09507aaf320140c3a9c
mahmudulturan <mahmudulhasanturan@gmail.com>
2024-04-21 18:25:20 +0600

    add: footer bg  and copyright section on footer

--- src/app/(root)/components/Shared/Footer/Footer.tsx
+++ src/app/(root)/components/Shared/Footer/Footer.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
-
+import './footer.css'
 const Footer = () => {
     return (
-        <div>
-            Footer
-        </div>
+        <footer className='footer-bg'>
+            <section className='min-h-[40vh]'>
+                
+            </section>
+            <section className='bg-seconderyCol py-10'>
+                <p className='text-center'>The Trio Dev LTD. © 2024 Trading as Gamers Paradise. All Rights Reserved.</p>
+            </section>
+        </footer>
     );
 };
 
