f8b7184b670dfe3af64c6b09dec861f06b987796
mahmudulturan <mahmudulhasanturan@gmail.com>
2024-04-21 23:12:51 +0600

    add: custom input component

---
+++ src/components/ui/Input/input.tsx
@@ -0,0 +1,25 @@
+import * as React from "react"
+
+import { cn } from "@/lib/utils"
+
+export interface InputProps
+  extends React.InputHTMLAttributes<HTMLInputElement> { }
+
+const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ className, type, ...props }, ref) => {
+    return (
+      <input
+        type={type}
+        className={cn(
+          "flex w-full rounded-[2px] bg-seconderyCol px-4 py-3 text-base  file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-textCol/90 focus-visible:outline-none outline-none focus-visible:ring-slate-950 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+          className
+        )}
+        ref={ref}
+        {...props}
+      />
+    )
+  }
+)
+Input.displayName = "Input"
+
+export { Input }
