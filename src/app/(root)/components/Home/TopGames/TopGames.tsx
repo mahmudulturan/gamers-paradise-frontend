240a34ea8a32562eaa7085a6ec1dd965225fb3a8
mahmudulturan <mahmudulhasanturan@gmail.com>
2024-04-27 01:24:05 +0600

    fix: section heading component's margin

--- src/app/(root)/components/Home/TopGames/TopGames.tsx
+++ src/app/(root)/components/Home/TopGames/TopGames.tsx
@@ -6,11 +6,9 @@ import { Button } from '@/components/ui/Button/button';
 import { FaCircleArrowRight } from "react-icons/fa6";
 
 const TopGames = () => {
-
-
     return (
         <div className='wrapper'>
-            <SectionHeading heading='Top Games Collection' subheading="Today's" />
+            <SectionHeading heading='Top Games Collection' subheading="Today's" className='py-12 my-0' />
             <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-6 lg:gap-8 xl:gap-10'>
                 {
                     topGames.map((game, indx) => <GameCard key={indx} game={game} />)
