595df4b026b378e162ce05e54f47b998413018d8
mahmudulturan <mahmudulhasanturan@gmail.com>
2024-04-29 02:32:51 +0600

    chore: change path of game route

--- src/app/(root)/components/Home/TopGames/GameCard.tsx
+++ src/app/(root)/components/Home/TopGames/GameCard.tsx
@@ -5,7 +5,7 @@ import React from 'react';
 
 const GameCard = ({ game }: { game: IGame }) => {
     return (
-        <Link href={game.route} className='rounded bg-seconderyCol hover:bg-primaryCol duration-300 flex items-center justify-center flex-col gap-2 pt-5 pb-4 group'>
+        <Link href={`/top-up${game.route}`} className='rounded bg-seconderyCol hover:bg-primaryCol duration-300 flex items-center justify-center flex-col gap-2 pt-5 pb-4 group'>
             <div className='overflow-hidden sm:w-40'>
                 <Image className='w-full group-hover:scale-105 group-hover:rotate-1 group-hover:saturate-150 transition-all duration-300 ' alt={`logo of ${game.name}`} src={game.logo} />
             </div>
